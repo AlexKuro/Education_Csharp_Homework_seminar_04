6e6f770099287c1fc76734cf3ce9e16ab4c9a009
AlexKuro <89215682813@mail.ru>
2022-11-21 20:36:29 +0300

    Task 25

---
+++ Homework/Program.cs
@@ -0,0 +1,81 @@
+﻿namespace Homework;
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Домашнее задание семинара №4
+
+        void Task_25()
+        {
+            /*Задача 25: Используя определение степени числа, напишите цикл, 
+            который принимает на вход два натуральных числа (A и B) и возводит число A в степень B.
+              3, 5 -> 243 (3⁵)
+            2, 4 -> 16 */
+            Console.WriteLine("                     ------Задача 25-------");
+            Console.WriteLine("Используя определение степени числа, напишите цикл, который принимает");
+            Console.WriteLine("на вход два натуральных числа (A и B) и возводит число A в степень B.");
+            Console.WriteLine();
+            Random rand = new Random();
+            int numberA = rand.Next(1, 9);
+            int numberB = rand.Next(1, 9);
+            Console.WriteLine($"Введено два натуральных числа: ({numberA}, {numberB})");
+            int length = numberB;
+            int sum = 1;
+            for (int i = 0; i < length; i++)
+            {
+                sum = sum * numberA;
+            }
+            Console.WriteLine($"   {numberA}, {numberB} -> {sum} ({numberA}^{numberB})");
+            Console.WriteLine("    Ответ: " + sum);
+
+        }
+
+        void Task_27()
+        {
+            /*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+               452 -> 11
+                82 -> 10
+              9012 -> 12*/
+
+            Console.WriteLine("                        ------Задача 27-------");
+            Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+            Console.WriteLine();
+            Console.Write("Введите число:-> ");
+
+            int container = Convert.ToInt32(Console.ReadLine());
+            int number = Math.Abs(container);       
+            int size = DigitsNumber(number);
+            int[] arrayNumber = new int[size + 1];
+            int count = size - 1;
+            int sum_1 = 0;
+            while (number > 0)
+            {
+                arrayNumber[count] = number % 10;
+                number /= 10;
+                count = count - 1;
+            }
+
+            int sum = 0;
+            for (int i = 0; i < size; i++)
+            {
+                sum = sum + arrayNumber[i];
+            }
+            Console.WriteLine($"Cуммa цифр в числе {container} -> {sum}");
+
+        }
+
+        int DigitsNumber(int number)
+        {
+            int count = 0;
+            while (number > 0)
+            {
+                number /= 10;
+                count = count + 1;
+            }
+            return count;
+        }
+
+        //Task_25();
+        Task_27();
+    }
+}
